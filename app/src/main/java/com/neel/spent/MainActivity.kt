874d774c4ad2d12deed260b1c9b2123e96b1f29a
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-04-14 21:43:54 +0530

    Updates

--- app/src/main/java/com/neel/spent/MainActivity.kt
+++ app/src/main/java/com/neel/spent/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.neel.spent
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val SMS_PERMISSION_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkSMSPermission()
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 replace(R.id.fragment_container, FirstFragment())
             }
+        }
+    }
+
+    private fun checkSMSPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), SMS_PERMISSION_CODE)
         }
     }
 }
